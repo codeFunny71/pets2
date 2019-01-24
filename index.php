ee213c9845ffbbec5db40737a581bd3ff13c2cc4
Marcus Absher <mabsher@mail.greenriver.edu>
2019-01-24 17:36:38 -0500

    Fixed form2 routing and animal session variable for results

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -7,6 +7,8 @@ error_reporting(E_ALL);
 //Require autoload
 require_once('vendor/autoload.php');
 
+$sound = "cluck";
+$_SESSION['sound'] = $sound;
 //create an instance of the Base class
 $f3 = Base::instance();
 
@@ -28,7 +30,7 @@ $f3->route('GET /@animal', function($f3,$params){
 //    print_r($params);
     $validAnimals=['chicken','dog','bird','lizard','pig'];
     $animal=$params['animal'];
-    $sound = "";
+
     //check validity
     if(!in_array($animal,$validAnimals)){
         $f3->error(404);
@@ -50,7 +52,8 @@ $f3->route('GET /@animal', function($f3,$params){
                 $sound="Oink!";
 
         }
-        echo $animal."".$sound;
+        //echo $animal."".$sound;
+        $_SESSION['sound'] = $sound;
     }
 
     ;
@@ -61,6 +64,31 @@ $f3->route('GET /order', function(){
     $view=new View();
     echo $view->render('views/form1.html');
 });
+
+$f3->route('POST /form2',
+    function() {
+        $_SESSION['animal'] = $_POST['animal'];
+        //echo "animal: ".$_SESSION['animal'];
+        $view = new View();
+//        echo '<pre>';
+//        print_r($_SESSION);
+//        print_r($_POST);
+//        echo '</pre>';
+        echo $view->render('views/form2.html');
+    });
+
+$f3->route('POST /results',
+    function($f3) {
+        $_SESSION['color'] = $_POST['color'];
+        //Create global variables
+        $f3->set('animal', $_SESSION['animal']);
+        $f3->set('color', $_SESSION['color']);
+        $f3->set('sound', $_SESSION['sound']);
+        //load a page using a Template
+        $template = new Template();
+//        print_r($_SESSION);
+        echo $template->render('views/results.html');
+    });
 
 //Run fat free
 $f3->run();
